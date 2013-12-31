d855a10739c78d3d1c698e66b0a1c52e8db22838
hidiree <hidiri.yayaya@gmail.com>
2014-01-01 08:39:26 +0900

    FluentTestを利用したテスト追加

---
+++ src/test/java/CalculatorBrowserAcceptanceTest.java
@@ -0,0 +1,31 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class CalculatorBrowserAcceptanceTest extends FluentTest {
+
+    @Override
+    public WebDriver getDefaultDriver() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+        return driver;
+    }
+
+    @Test
+    public void _1plus2_equal_3() throws Exception {
+        goTo("http://localhost:8080/devkan-calc/index.html");
+        fill("#x1").with("1");
+        find("#op").find("option[value='add']").click();
+        fill("#x2").with("2");
+        click("#send");
+        assertThat(find("#result").getText()).isEqualTo("3");
+    }
+
+}
