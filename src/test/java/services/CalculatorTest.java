d48178b3de5aa8365791eeb9ecaff9d7edb936bd
hidiree <hidiri.yayaya@gmail.com>
2014-01-01 08:54:57 +0900

    リファクタリング

--- src/test/java/services/CalculatorTest.java
+++ src/test/java/services/CalculatorTest.java
@@ -7,16 +7,16 @@ import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator sut = new Calculator();
+
     @Test
     public void testAdd() {
-        Calculator sut = new Calculator();
         String actual = sut.add(1, 1);
         assertThat(actual, is("2"));
     }
 
     @Test
-    public void testMinus() throws Exception {
-        Calculator sut = new Calculator();
+    public void testSubtract() throws Exception {
         String actual = sut.subtract(3, 2);
         assertThat(actual, is("1"));
     }
